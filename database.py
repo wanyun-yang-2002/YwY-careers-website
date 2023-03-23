985a3233a0a79958ffc427a54bcba3a7736aad2c
wanyun-yang-2002 <wanyun.yang@outlook.com>
2023-03-23 19:51:01 +0800

    dynamic pages for each job

--- database.py
+++ database.py
@@ -20,17 +20,14 @@ def load_jobs_from_db():
         jobs.append(row._mapping)
   return jobs
 
-# with engine.connect() as conn:
-#     result = conn.execute(text("select * from jobs"))
-#     jobs = []
-#     for row in result.all():
-#         print(row._mapping)
-#         print(type(row))
-
-# with engine.connect() as conn:
-#     result = conn.execute(text("select * from jobs"))
-    
-#     result_dicts = []
-#     for row in result.all():
-#         result_dicts.append(dict(row))
-#     print(result_dicts)
+def load_job_from_db(id):
+   with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+       return None
+    else:
+       return dict(rows[0])
